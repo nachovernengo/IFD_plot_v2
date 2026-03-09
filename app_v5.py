deb1fca46acd18b4e10d37c4b795a1f986953ea3
Ignacio Vernengo Lezica <63291368+nachovernengo@users.noreply.github.com>
2026-03-10 09:58:06 +1000

    Update app_v5.py
    Added template for data to be uploaded

--- app_v5.py
+++ app_v5.py
@@ -267,6 +267,14 @@ def plot_ifd(df, ordered_cols, labels, bases):
 st.set_page_config(page_title="IFD Plotter", layout="centered")
 st.title("IFD Plotter (log–log) v2")
 st.caption("Upload your IFD Excel file (or suitable CSV). AEP curves (e.g. 10%, 10%CC) will be matched in color. Legend labels are compact.")
+st.caption("Download template for data here")
+with open("IFD_template.xlsx", "rb") as file:
+    st.download_button(
+        label="Download Excel Template",
+        data=file,
+        file_name="IFD_template.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
 
 uploaded = st.file_uploader("Upload your IFD_data.xlsx or .csv", type=["xlsx", "xls", "csv"])
 
